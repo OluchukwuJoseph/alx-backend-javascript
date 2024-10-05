a019cd5d6a5017bf4b604bc46345f07be67e4063
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-10-05 15:43:36 +0100

    Fix: functions of Task 10 and 14

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,15 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let i = 0;
-  let string = '';
-
-  for (const value of reportWithIterator) {
-    if (i === 0) {
-      string = value;
-    } else {
-      string = `${string} | ${value}`;
-    }
-    i += 1;
-  }
-
-  return string;
+  return reportWithIterator.data.join(' | ');
 }
