e57c324e88e89e917cb58cdc6a4196b2ace20e45
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-10-07 04:07:21 +0100

    fix: functions of Task 6 and 10

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,12 +3,10 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   try {
     const uploadPromise = await uploadPhoto()
-      .then((response) => response)
-      .catch((error) => error);
+      .then((response) => response);
 
     const userPromise = await createUser()
-      .then((response) => response)
-      .catch((error) => error);
+      .then((response) => response);
 
     return { photo: uploadPromise, user: userPromise };
   } catch (error) {
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,16 +5,14 @@ export default async function handleProfileSignup(firstName, lastName, filename)
   const array = [];
   try {
     const firstPromise = await signUpUser(firstName, lastName)
-      .then((response) => response)
-      .catch((error) => error);
+      .then((response) => response);
     array.push({ status: 'fulfilled', value: firstPromise });
   } catch (error) {
     array.push({ status: 'rejected', value: error.toString() });
   }
   try {
     const secondPromise = await uploadPhoto(filename)
-      .then((response) => response)
-      .catch((error) => error);
+      .then((response) => response);
     array.push({ status: 'fulfilled', value: secondPromise });
   } catch (error) {
     array.push({ status: 'rejected', value: error.toString() });
