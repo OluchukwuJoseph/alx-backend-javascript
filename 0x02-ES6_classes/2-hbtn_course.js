d78037b991cc30b62af3e1779713f159affeb002
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-10-09 06:45:42 +0100

    Added files for task 0, 1, 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,36 @@
+export default class HolbertonCourse {
+  constructor (name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+  set name(x) {
+    if (typeof x !== "string") throw TypeError('Name must be a string');
+    this._name = x;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(x) {
+    if (typeof x !== "number") throw TypeError('Length must be a number');
+      this._length = x;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(x) {
+    if (!Array.isArray(x)) throw TypeError('Students must be an array');
+    x.forEach((student) => {
+      if (typeof student !== 'string') throw TypeError('Students must be an array of strings');
+    });
+    this._students = x;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,11 @@
+# 0x02-ES6_classes
+Welcome to the 0x02-ES6_classes project! This repository focuses on learning and applying classes in JavaScript, which were introduced in ES6 (ECMAScript 2015). Classes provide a cleaner and more structured way to work with object-oriented programming (OOP) in JavaScript.
+
+## Project Overview
+This project delves into JavaScript classes, which are a blueprint for creating objects. While JavaScript has always supported OOP, ES6 introduced the class keyword to make object creation and inheritance easier to understand. This project helped me learn how to:
+
+- Define classes and constructors.
+- Work with class methods.
+- Implement inheritance with subclasses.
+- Use static methods.
+- Leverage getter and setter methods for encapsulation.
