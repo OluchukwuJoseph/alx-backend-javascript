c85c0fc76434268a7826759a2eca2177f26d25cd
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-10-13 07:30:33 +0100

    Added file for task 100

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  if (weakMap.has(endpoint)) {
+    const queries = weakMap.get(endpoint);
+    if (queries >= 5) throw Error('Endpoint load is high');
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+}
+
+export { queryAPI, weakMap };
