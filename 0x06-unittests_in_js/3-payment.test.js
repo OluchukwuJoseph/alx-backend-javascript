5bf833e6dad048cb99b17cdb2a0ee61c45757c65
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-11-30 03:45:09 +0100

    Resolved issues in task 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,6 +1,6 @@
 const { describe, it } = require('mocha');
-const { expect } = require('chai');
-const { spy } = require('sinon');
+const assert = require("assert");
+const { spy, stub } = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
@@ -9,22 +9,14 @@ describe('sendPaymentRequestToApi', function () {
   const mySpy = spy(Utils, "calculateNumber");
 
   it('Check the arguments called with Utils.calculateNumber', function () {
-    const firstTotal = sendPaymentRequestToApi(300, 35);
-    expect(mySpy.args).to.equal([['SUM', 300, 35]]);
+    sendPaymentRequestToApi(300, 35);
+    assert.equal(mySpy.args, [['SUM', 300, 35]]);
   });
   it('Check if Utils.calculateNumber was called', function () {
-    const total = sendPaymentRequestToApi(300, 35);
-    expect(mySpy.called).to.be.true;
-    expect(total).to.be.equal(mySpy(300, 35));
-  });
-  it('Check if Utils.calculateNumber was called Once', function () {
-    const total = sendPaymentRequestToApi(300, 35);
-    expect(mySpy.calledTwice).to.be.true;
-    expect(total).to.be.equal(mySpy(300, 35));
-  });
-  it('Check if Utils.calculateNumber was called Twicce', function () {
-    const total = sendPaymentRequestToApi(300, 35);
-    expect(mySpy.calledThrice).to.be.true;
-    expect(total).to.be.equal(mySpy(300, 35));
+    const myStub = stub(Utils, "calculateNumber");
+    myStub.withArgs("SUM", 300, 35).returns(335);
+    sendPaymentRequestToApi(300, 35);
+    assert.equal(mySpy.called, true);
+    assert.equal(335, mySpy("SUM", a, b));
   });
 });
